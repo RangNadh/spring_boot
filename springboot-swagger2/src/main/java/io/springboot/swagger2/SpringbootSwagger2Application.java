022fb84fee7b99ba7dcd2874e1c074fd466ad6ac
RangNadh <ranganadh1509@gmail.com>
2022-07-06 12:15:05 +0530

    commit

--- springboot-swagger2/src/main/java/io/springboot/swagger2/SpringbootSwagger2Application.java
+++ springboot-swagger2/src/main/java/io/springboot/swagger2/SpringbootSwagger2Application.java
@@ -2,7 +2,12 @@ package io.springboot.swagger2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -14,5 +19,14 @@ public class SpringbootSwagger2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootSwagger2Application.class, args);
 	}
-
+	
+	@Bean
+	public Docket swaggerCongiguration() {
+		// Return a prepared Docket instance
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/*"))
+				.apis(RequestHandlerSelectors.basePackage("io.springboot.swagger2"))
+				.build();
+	}
 }
