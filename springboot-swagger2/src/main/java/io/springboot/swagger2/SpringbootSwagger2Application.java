21b37c8cddfd67410a797f86b7f45a24471a29f0
RangNadh <ranganadh1509@gmail.com>
2022-07-06 18:10:06 +0530

    commit

--- springboot-swagger2/src/main/java/io/springboot/swagger2/SpringbootSwagger2Application.java
+++ springboot-swagger2/src/main/java/io/springboot/swagger2/SpringbootSwagger2Application.java
@@ -1,11 +1,14 @@
 package io.springboot.swagger2;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -27,6 +30,19 @@ public class SpringbootSwagger2Application {
 				.select()
 				.paths(PathSelectors.ant("/api/*"))
 				.apis(RequestHandlerSelectors.basePackage("io.springboot.swagger2"))
-				.build();
+				.build()
+				.apiInfo(apiDetails());
+	}
+	
+	private ApiInfo apiDetails() {
+		return new APiInfo(
+				"Address book API",
+				"Sample API for Practice",
+				"1.0",
+				"Free to use",
+				new springfox.documentation.service.Contact("ranganadh", "http://ranganadh.io", "a@b.com"),
+				"API License",
+				"http://ranganadh.io",
+				Collections.emptyList());
 	}
 }
